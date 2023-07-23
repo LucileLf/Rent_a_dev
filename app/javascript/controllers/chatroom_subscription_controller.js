58bc1bc92ce61016279d040b22a03d510b26bfb8
Lucile Lefebvre <lucile.c.lefebvre@gmail.com>
2023-07-23 20:23:57 +0200

    display new message, scroll, clear message input

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -4,14 +4,30 @@ import { createConsumer } from "@rails/actioncable"
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
   static values = { chatroomId: Number }
-  static targets = ["messages"]
+  static target = ["messages"];
 
   connect() {
     this.channel = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => this.element.insertAdjacentHTML("beforeend", data) }
-
+      // { received: data => this.messagesTargets.insertAdjacentHTML("beforeend", data) }
+      { received: data => this.insertMessageAndScroll(data) }
     )
     console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
   }
+  disconnect () {
+console.log("Unsubscribed from the chatroom with the id ${this.chatroomIdValue}.`")
+this.channel.unsubscribe()
+  }
+
+
+  insertMessageAndScroll(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+    //this.newmessageTarget.innerText = "";
+  }
+  resetForm(event) {
+    event.target.reset()
+  }
+
+
 }
